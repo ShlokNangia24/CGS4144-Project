931c6993313be598596a3f6cbf2878be6c576541
ShlokNangia24 <74503757+ShlokNangia24@users.noreply.github.com>
2023-09-27 16:36:51 -0400

    Add files via upload

--- Assignment_2.R
+++ Assignment_2.R
@@ -1,3 +1,5 @@
+# Part One ----------------------------------------------------------------
+
 data_dir <- file.path("R/CGS_data/data","SRP073813")
 data_file <- file.path(data_dir,"SRP073813_Entrez_IDs.tsv")
 metadata_file <- file.path(data_dir,"metadata_SRP073813.tsv")
@@ -10,12 +12,13 @@ library(conflicted)
 conflicts_prefer(dplyr::select)
 drops <- c("Ensembl","all_entrez_ids")
 df <- df[ ,!(names(df) %in% drops)]
+df <- na.omit(df)
 
 # Remove non-numeric columns from df
 df_numeric <- df %>% select(where(is.numeric))
-
 # Log-scale the data (adding a small constant to avoid log(0))
-log_df <- log2(df_numeric - min(df_numeric + 2))
+df_numeric <- (df_numeric - min(df_numeric) +2)
+log_df <- log2(df_numeric)
 
 # Calculate per-gene median expression ranges
 ranges <- apply(log_df, 1, function(row) {
@@ -32,24 +35,16 @@ plot <- ggplot(data.frame(ranges), aes(x=ranges)) +
 
 print(plot)
 
-# Part two ----------------------------------------------------------------
-cts <- as.matrix(df)
-coldata <- readr::read_tsv(metadata_file)
-coldata <- coldata[,c("refinebio_subject","refinebio_accession_code")]
-coldata$refinebio_subject <- factor(coldata$refinebio_subject)
-coldata$refinebio_accession_code <- factor(coldata$refinebio_accession_code)
-rownames(coldata) <- sub("fb", "", rownames(coldata))
-all(rownames(coldata) %in% colnames(cts))
-all(rownames(coldata) == colnames(cts))
-cts <- cts[, rownames(coldata)]
-all(rownames(coldata) == colnames(cts))
-library("DESeq2")
-dds <- DESeqDataSetFromMatrix(countData = cts,
-                              colData = coldata,
-                              design = ~ condition)
-dds
-vsd <- vst(dds, blind=FALSE)
-library("vsn")
-plotPCA(vsd, intgroup=c("condition", "type"))
-
-
+# Part five ----------------------------------------------------------------
+data_file <- file.path(data_dir,"nacc_log.csv")
+metadata_file <- file.path(data_dir,"nacc_meta.csv")
+df <- read.csv(data_file)
+df_meta <- read.csv(metadata_file)
+gostres <- gost(query = df$X, 
+                organism = "hsapiens", ordered_query = FALSE, 
+                multi_query = FALSE, significant = TRUE, exclude_iea = FALSE, 
+                measure_underrepresentation = FALSE, evcodes = FALSE, 
+                user_threshold = 0.05, correction_method = "g_SCS", 
+                domain_scope = "annotated", custom_bg = NULL, 
+                numeric_ns = "", sources = NULL, as_short_link = FALSE, highlight = TRUE)
+gostplot(gostres, capped = FALSE, interactive =FALSE)
